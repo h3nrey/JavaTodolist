17576a81011986d485f6c1f5a9bcaac700516a69
h3nrey <pedro.novaes.education@gmail.com>
2023-10-13 11:00:23 -0300

    adding docketfile

--- server/src/main/java/br/com/h3nrey/todolist/user/UserController.java
+++ server/src/main/java/br/com/h3nrey/todolist/user/UserController.java
@@ -33,6 +33,6 @@ public class UserController {
 
         // Save on database
         var userCreated = this.userRepository.save(userModel);
-        return ResponseEntity.status(HttpStatus.CREATED).body(userCreated);
+        return ResponseEntity.status(HttpStatus.OK).body(userCreated);
     }
 }
